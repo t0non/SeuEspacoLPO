bf8c5eff727e3ab446fba79f20f1a82e32bd4357
Eduardo Tonon <eduardosoarestonon@gmail.com>
2026-04-12 18:18:33 -0300

    Mensagem explicando o que você alterou

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from 'next';
 import { Instrument_Sans } from 'next/font/google';
-import Script from 'next/script';
 import './globals.css';
 import { cn } from '@/lib/utils';
 import { Toaster } from '@/components/ui/toaster';
 import Footer from '@/components/footer';
 import WhatsappFab from '@/components/whatsapp-fab';
 import JsonLd from '@/components/JsonLd';
+import Script from 'next/script';
 
 const instrumentSans = Instrument_Sans({
   subsets: ['latin'],
@@ -93,9 +93,26 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" className={cn(instrumentSans.variable)}>
       <head>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-M8Q3XJ82');
+          `}
+        </Script>
         <JsonLd />
       </head>
       <body className={cn('font-sans antialiased bg-background text-foreground')}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-M8Q3XJ82"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <Toaster />
         <main>{children}</main>
         <Footer />
