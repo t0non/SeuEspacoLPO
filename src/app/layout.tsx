b7d4c35654a530e734962ebcb23838dcc8dc351e
Eduardo Tonon <eduardosoarestonon@gmail.com>
2026-04-12 19:25:36 -0300

    Mensagem explicando o que você alterou

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -93,15 +93,15 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" className={cn(instrumentSans.variable)}>
       <head>
-        <Script id="google-tag-manager" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-M8Q3XJ82');
-          `}
-        </Script>
+            })(window,document,'script','dataLayer','GTM-M8Q3XJ82');`,
+          }}
+        />
         <JsonLd />
       </head>
       <body className={cn('font-sans antialiased bg-background text-foreground')}>
